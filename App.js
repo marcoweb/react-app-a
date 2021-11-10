ac6b9e5a2aae88c0663047fec92abd15dead9b55
marcoweb <marcocarvalho.web@gmail.com>
2021-11-10 12:22:35 +0000

    Criação da Aplicação

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá Mundo!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6a6a6a',
     alignItems: 'center',
     justifyContent: 'center',
   },
